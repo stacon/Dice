872f8e7259f1f182a7ca9bdfc626d2e6f4a350f4
Constantinos Stathis <k.stathis@afse.eu>
2020-02-26 12:23:13 +0200

    Scemantic improvments and package.json init

--- Roll.js
+++ Roll.js
@@ -3,48 +3,48 @@
 const Roll = (() => {
   /**
    * Returns a number between a min value (inclusive) and a max value (inclusive)
-   * @param {number} max
-   * @param {number} min
+   * @param   {number} max
+   * @param   {number} min
    * @returns {number}
    */
   const getANumberBetweenMinAndMax = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
 
   /**
-   * Rolls a die with a min = 1 and max limit and returns the result
-   * @param {number} max the maximum allowd roll number
-   * @returns {number} the result of the die
+   * Emulates the roll of a single die with customer number of dimensions
+   * @param   {number} dimensions the maximum allowed roll number (die dimensions)
+   * @returns {number} the result of rolling the die
    */
-  const rollDieWithMin1 = (max) => getANumberBetweenMinAndMax(1, max);
+  const rollADie = (dimensions) => getANumberBetweenMinAndMax(1, dimensions);
 
   /**
-   * Rolls a die of the same dimentions as many times as the second argument (times) (default: 1)
-   * Eg. rollDTimes(20, 5) will return the result of rolling 5 * d20 ( 5 times 20 dimentional dice)
-   * @param {number} sides the numbers of the dice's dimentions (sides)
-   * @param {number} times the number of times to roll the die
+   * Rolls a die of the same dimensions as many times as the second argument (times) (default: 1)
+   * Eg. rollADieTimes(20, 5) will return the result of rolling 5 * d20 ( 5 times 20 dimensional dice)
+   * @param   {number} dimensions the numbers of the dice's dimensions
+   * @param   {number} times the number of times to roll the die
    * @returns {number} the result of the dice
    */
-  const rollDTimes = (sides, times = 1) => Array(times).fill(null).reduce(result => result + rollDieWithMin1(sides), 0);
+  const rollADieTimes = (dimensions, times = 1) => Array(times).fill(null).reduce(result => result + rollADie(dimensions), 0);
 
   /**
    * There are three ways to use this function to roll from one single die, or a mixed set of them
-   * @param  {number[]} args Example 1: Roll.D(20) will return the result of rolling a d20 ( a 20 dimentional dice)
-   * @param  {number[]} args Example 2: Roll.D(20, 5) will return the result of rolling 5 * d20 ( 5 times 20 dimentional dice)
-   * @param  {Object}   args Example 3: Roll.D({20: 5, 12: 3, 10: 2}) will return the result of rolling 5 * d20 + 3 * d12 + 2 * d10
-   * @returns {number} the accumulated result of the dice roll or returns 0 in case of incorrect input
+   * @param   {number[]} args Example 1: Roll.D(20) will return the result of rolling a d20 ( a 20 dimensional dice)
+   * @param   {number[]} args Example 2: Roll.D(20, 5) will return the result of rolling 5 * d20 ( 5 times 20 dimensional dice)
+   * @param   {Object}   args Example 3: Roll.D({20: 5, 12: 3, 10: 2}) will return the result of rolling 5 * d20 + 3 * d12 + 2 * d10
+   * @returns {number}   the accumulated result of the dice roll or 0 in case of incorrect input
    */
   const D = (...args) => {
     if (!args || !args.length) return 0;
     if (args.length === 1 && typeof args[0] === 'object') {
       const [rolls] = args;
-      return Object.keys(rolls)
+      return Object.entries(rolls)
                 .reduce(
-                    (accumulatedRolls, currentRollKey) => accumulatedRolls + rollDTimes(currentRollKey, rolls[currentRollKey]), 0);
+                    (accumulatedRolls, [dimensions, times]) => accumulatedRolls + rollADieTimes(dimensions, times), 0);
     }
 
     if (args.length <= 2 && typeof args[0] === 'number') {
-      let [sides, times] = args;
+      let [dimensions, times] = args;
       times = typeof times === 'number' ? times : 1;
-      return rollDTimes(sides, times);
+      return rollADieTimes(dimensions, times);
     }
 
     return 0;
@@ -54,5 +54,7 @@ const Roll = (() => {
     D,
   };
 })();
+
+console.log(Roll.D({20: 4}));
 
 exports.default = Roll;
