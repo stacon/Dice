f2f4e7c291de89674659a27188ce486b916f68fe
Constantinos Stathis <constantinos-@hotmail.com>
2020-10-01 14:29:06 +0300

    Removed unused files from dist version

--- src/libs/getNumberBetweenMinAndMax.test.js
+++
@@ -1,9 +0,0 @@
-import { getNumberBetweenMinAndMax } from '../libs/getNumberBetweenMinAndMax';
-
-describe('getNumberBetweenMinAndMax tests, ', () => {
-  it('return a number within the range', () => {
-    const randomNumber = getNumberBetweenMinAndMax(1, 3);
-    const validResult = randomNumber >= 1 && randomNumber <= 3;
-    expect(validResult).toBeTruthy();
-  });
-});
