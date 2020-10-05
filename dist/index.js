c2b6a55ad357f06d18d22d0cd6a546f26a5f6429
Constantinos Stathis <constantinos-@hotmail.com>
2020-10-05 22:49:27 +0300

    Updated gitignore

--- dist/index.js
+++ dist/index.js
@@ -31,18 +31,5 @@ var _sum = _interopRequireDefault(require("./sum"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-// Create a dice set
-var D20 = (0, _d["default"])(20); // Sigle die roll
-
-console.log((0, _roll["default"])(D20)); // should log a number between 1 - 20 like a d20 roll would.
-// Multiple dice roll of the same dimensions
-
-var resultsArr = _roll["default"].times(5)(D20); // returns an array of numbers between 1 - 20
-
-
-console.log(resultsArr); // should log e.g [ 19, 20, 1, 5, 11 ]
-
-console.log((0, _sum["default"])(resultsArr)); // should return the sum of results are e.g. 56
-
 var _default = _roll["default"];
 exports["default"] = _default;
